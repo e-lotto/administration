8a68c94fc70c9b5ddd9af116c2e0e2215243c777
Administrator <ykoulate@ldfgroupe.com>
2018-08-14 17:48:12 +0000

    test 30/04/2018

--- src/main/java/com/elotto/administration/administration/AdministrationApplication.java
+++ src/main/java/com/elotto/administration/administration/AdministrationApplication.java
@@ -9,6 +9,6 @@ public class AdministrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdministrationApplication.class, args);
                 
-                
+                //**
 	}
 }
